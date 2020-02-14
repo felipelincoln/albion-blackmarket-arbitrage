cc36d19b2a908d5379dd056bbf8a4e36fe1286cd
Felipe Lincoln <felipelincoln@usp.br>
2020-02-14 04:10:06 -0300

    add itemname.py

---
+++ py/itemname.py
@@ -0,0 +1,19 @@
+#python3 py/itemname.py urls/<file>
+import requests
+import sys
+
+api = 'https://api.hackertarget.com/pagelinks/?q='
+urls = open(sys.argv[1], 'r').read().split()
+items = []
+
+print(urls)
+
+
+for url in urls:
+	response = requests.get(api+url)
+	it = [x[42:] for x in response.text.split() if 'https://www.albiononline2d.com/en/item/id/' in x]
+	items += it
+
+for item in items:
+	print(item)
+
