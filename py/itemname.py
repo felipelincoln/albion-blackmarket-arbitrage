e931bab43d1b7faa12952c910ecfead7427a229b
Fred Chevitarese <fchevitarese@gmail.com>
2020-02-25 10:38:22 -0300

    Change identation from tabs to space. Add requirements and Pipfile for creation of a virtual_env

--- py/itemname.py
+++ py/itemname.py
@@ -10,10 +10,9 @@ print(urls)
 
 
 for url in urls:
-	response = requests.get(api+url)
-	it = [x[42:] for x in response.text.split() if 'https://www.albiononline2d.com/en/item/id/' in x]
-	items += it
+    response = requests.get(api+url)
+    it = [x[42:] for x in response.text.split() if 'https://www.albiononline2d.com/en/item/id/' in x]
+    items += it
 
 for item in items:
-	print(item)
-
+    print(item)
--- py/retriever.py
+++ py/retriever.py
@@ -12,17 +12,17 @@ response_blackmarket = requests.get(api + items + tag + 'blackmarket').json()
 val = {}
 
 for each in response_caerleon:
-	if each['sell_price_min']:
-		val[each['item_id'] + '#' + str(each['quality'])] = [each['sell_price_min'], 0]
+    if each['sell_price_min']:
+        val[each['item_id'] + '#' + str(each['quality'])] = [each['sell_price_min'], 0]
 
 for each in response_blackmarket:
-	k = each['item_id'] + '#' + str(each['quality'])
-	if k in val.keys(): 
-		val[k][1] = each['buy_price_max']
+    k = each['item_id'] + '#' + str(each['quality'])
+    if k in val.keys():
+        val[k][1] = each['buy_price_max']
 
 tax = 0.06
 i = 0
 for each in sorted(val.items(), key=lambda x: (x[0], -x[1][1]*(1-tax)+x[1][0])):
-	if each[1][1]*(1-tax) > each[1][0]:
-		print(f'[{i}]', each[0], each[1], round(each[1][1]*(1-tax)-each[1][0]))
-		i += 1
+    if each[1][1]*(1-tax) > each[1][0]:
+        print(f'[{i}]', each[0], each[1], round(each[1][1]*(1-tax)-each[1][0]))
+        i += 1
